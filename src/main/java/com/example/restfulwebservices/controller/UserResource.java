07e6fda9c83a2634fb6f9ace9f507ea6c376d140
Remigio Fernandez <rfernandez@nisum.com>
2019-01-17 14:18:42 -0300

    Added User Validation and HATEOAS

--- src/main/java/com/example/restfulwebservices/controller/UserResource.java
+++ src/main/java/com/example/restfulwebservices/controller/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.restfulwebservices.controller;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import com.example.restfulwebservices.model.User;
 import com.example.restfulwebservices.service.UserDaoService;
 import com.example.restfulwebservices.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -28,7 +32,7 @@ public class UserResource {
     }
 
     @PostMapping
-    public ResponseEntity<Object> createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder
@@ -40,12 +44,20 @@ public class UserResource {
     }
 
     @GetMapping("/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public Resource<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user==null){
             throw new UserNotFoundException("id-"+id);
         }
-        return user;
+
+        Resource<User> resource = new Resource<>(user);
+
+        ControllerLinkBuilder linkTo=
+        linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+        //HATEOAS
+        return resource;
     }
 
     @DeleteMapping("/{id}")
