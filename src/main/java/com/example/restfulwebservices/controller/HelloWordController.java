54d6297d618fcbc8a7a09aeac7e350963b78a75f
Remigio Fernandez <rfernandez@nisum.com>
2019-01-17 14:54:08 -0300

    Internalization

--- src/main/java/com/example/restfulwebservices/controller/HelloWordController.java
+++ src/main/java/com/example/restfulwebservices/controller/HelloWordController.java
@@ -1,16 +1,29 @@
 package com.example.restfulwebservices.controller;
 
 import com.example.restfulwebservices.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWordController {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-internalization")
+    public String goodMorning(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 
     @GetMapping("/hello-world-bean")
